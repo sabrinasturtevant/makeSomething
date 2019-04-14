30b6d10a852d702248a746fa3c8ef328b978530a
Sabrina Sturtevant <sabrinasturtevant@host-250-253.arjogro.jonesboro.ar.us.clients.pavlovmedia.net>
2019-04-14 16:17:19 -0500

    i made something

--- makeSomething/ViewController.swift
+++ makeSomething/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var fortuneLabel: UILabel!
+    
+    var randomResponse = ["No", "Try again later", "Yes", "It is uncertain", "Never", "Why did you even bother asking?", "Consult the stars instead"]
+    
+    
+    @IBAction func fortuneButtonPressed(_ sender: Any){
+        
+        if (userTextField.text?.isEmpty)! {
+            fortuneLabel.text = "Ask a question"
+        } else {
+            fortuneLabel.text = randomResponse.randomElement()
+        }
 
+    
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
